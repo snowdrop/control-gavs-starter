fb0394d1512e30ee4a1f577159644b6a28de1638
Charles Moulliard <cmoulliard@redhat.com>
2018-06-11 18:00:39 +0200

    Add files to be ignored

---
+++ src/main/java/me/snowdrop/CheckStarter.java
@@ -0,0 +1,50 @@
+package me.snowdrop;
+
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenCoordinate;
+import sun.jvm.hotspot.runtime.Bytes;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CheckStarter {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        String STARTERS_FILE = "starters-list.txt";
+        String SEPARATOR_LINE = new String(new char[100]).replace("\0", "=");
+        String NEW_LINE = System.getProperty("line.separator");
+
+        Files.createDirectories(Paths.get("./generated"));
+        FileWriter fw = new FileWriter("./generated/gavs.txt",true);
+
+        Path inPath = Paths.get(ClassLoader.getSystemResource(STARTERS_FILE).toURI());
+        List<String> lines = Files.readAllLines(inPath, StandardCharsets.UTF_8);
+
+        StringBuilder bw = new StringBuilder();
+        for (String line : lines) {
+            bw.append(SEPARATOR_LINE).append(NEW_LINE);
+            bw.append("Spring Boot Starter : " + line).append(NEW_LINE);
+            bw.append(SEPARATOR_LINE).append(NEW_LINE);
+
+            for (MavenCoordinate gav : collectGAVs(line)) {
+                bw.append(gav.getGroupId() + ':' + gav.getArtifactId() + ":" + gav.getVersion()).append(NEW_LINE);
+            }
+            bw.append(NEW_LINE);
+        }
+        fw.write(bw.toString());
+        fw.close();
+    }
+
+    static List<MavenCoordinate> collectGAVs(String gav) {
+        return Maven.resolver()
+                .resolve(gav)
+                .withTransitivity()
+                .asList(MavenCoordinate.class);
+    }
+
+}
