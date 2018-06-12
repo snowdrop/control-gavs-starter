1027fa5398718ac074b2b26a90088878341f930a
Charles Moulliard <cmoulliard@redhat.com>
2018-06-12 15:04:09 +0200

    Refactor project

--- src/main/java/me/snowdrop/route/FeedBean.java
+++ src/main/java/me/snowdrop/route/FeedBean.java
@@ -3,8 +3,11 @@ package me.snowdrop.route;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import org.apache.camel.Body;
+import org.apache.camel.Headers;
 
-public class FilterBean {
+import java.util.Map;
+
+public class FeedBean {
 
     static public String newLine() {
         return System.getProperty("line.separator");
@@ -19,7 +22,7 @@ public class FilterBean {
         return firstEntry.getTitle().contains("spring-");
     }
 
-    public String outputResult(@Body SyndFeed feed) {
+    public String outputResult(@Body SyndFeed feed, @Headers Map<Object, Object> headers) {
         StringBuffer sbf = new StringBuffer();
         SyndEntry entry = (SyndEntry) feed.getEntries().get(0);
         sbf.append(lineSeparator()).append(newLine());
@@ -28,6 +31,9 @@ public class FilterBean {
         sbf.append("Date : " + entry.getPublishedDate()).append(newLine());
         sbf.append("Description : " + entry.getDescription()).append(newLine());
         sbf.append(lineSeparator()).append(newLine());
+
+        headers.put("CamelfileName",entry.getTitle());
+
         return sbf.toString();
     }
 }
--- src/main/java/me/snowdrop/route/RssToCVEFile.java
+++ src/main/java/me/snowdrop/route/RssToCVEFile.java
@@ -1,24 +1,19 @@
 package me.snowdrop.route;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.io.FileWriter;
-
 @Component
-public class RssToCVE extends RouteBuilder {
+public class RssToCVEFile extends RouteBuilder {
 
     String rssURL = "https://pivotal.io/security/rss?splitEntries=true";
-
-    FilterBean filterBean;
 
     @Override
     public void configure() throws Exception {
         from("rss:" + rssURL)
-          .filter()
-            .method(FilterBean.class,"titleContainsSpringProject")
-          .bean(FilterBean.class,"outputResult")
+          .bean(FeedBean.class,"outputResult")
+            .log(LoggingLevel.DEBUG,"File name : ${header.CamelfileName}")
           .to("file://generated/cve");
     }
 }
