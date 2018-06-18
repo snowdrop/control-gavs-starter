1dd067a541142fc9c2a925cf59f96d10bbb1dca0
Charles Moulliard <cmoulliard@redhat.com>
2018-06-18 08:54:23 +0200

    Add groovy script

---
+++ spring_projects.groovy
@@ -0,0 +1,21 @@
+final url = "https://gist.githubusercontent.com/cmoulliard/1457fdd3fc505a5e1f9878e8f842365a/raw/756a141c68169f8dcc6c55e8eece9119c6769012/gistfile1.txt".toURL()
+final text = url.text
+
+final File projectsFile = new File("generated/spring_projects.txt")
+
+final result =
+        text
+          .split( '\n' )
+          .findAll { it.contains( '!' ) }
+          .collect {
+            final index = it.indexOf('!')
+            return it.substring(index + 1)
+        }
+        .unique()
+        .sort()
+
+result.each {
+    if (it.contains('springframework')) {
+        projectsFile << ("${it}\n");
+    }
+}
